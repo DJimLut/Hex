ace109a06e48e909b85acb2487075c1d990364a3
David Jimenez <djimenez@murraystate.edu>
2023-04-23 18:40:46 -0400

    move to accurate package

--- src/main/LogicalComponents/Move.java
+++ src/main/LogicalComponents/Move.java
@@ -1,5 +1,27 @@
 package main.LogicalComponents;
 
+import main.Players.*;
+
 public class Move {
-    
+    private Player player;
+    private int x;
+    private int y;
+
+    public Move(Player player, int x, int y) {
+        this.player = player;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 }
