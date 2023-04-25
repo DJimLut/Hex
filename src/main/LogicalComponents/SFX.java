eaf6d9f5d37304d034531c1a139acad5506fc2ab
Mason Richardson <mrichardson3065@gmail.com>
2023-04-25 16:57:49 -0500

    Update SFX.java

--- src/main/LogicalComponents/SFX.java
+++ src/main/LogicalComponents/SFX.java
@@ -25,21 +25,42 @@ public class SFX {
     //Plays tile click sound effect
     public void playTileClick() {
         filePath = "audio_files\\tile_click.wav";
-
-
+        File file = new File(filePath);
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(file));
+            clip.start();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 
     //Plays win sound effect
     public void playWin() {
         filePath = "audio_files\\win.wav";
-
-
+        File file = new File(filePath);
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(file));
+            clip.start();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 
     //Plays lose sound effect
     public void playLose() {
         filePath = "audio_files\\lose.wav";
-
-
+        File file = new File(filePath);
+        try{
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(file));
+            clip.start();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 }
