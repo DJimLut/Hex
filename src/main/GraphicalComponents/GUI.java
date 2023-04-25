74cf17569139606486b25751586a8ce8ae7e6f7a
jaycoug <99698536+jaycoug@users.noreply.github.com>
2023-04-25 07:10:16 -0500

    Update GUI.java
    Title and names/assignment details added to title screen. Decorative hex gif file "GIVv.gif" accessed from lib folder.

--- src/main/GraphicalComponents/GUI.java
+++ src/main/GraphicalComponents/GUI.java
@@ -7,14 +7,34 @@ import main.Boards.*;
 
 public class GUI extends JFrame implements ActionListener {
 	private JPanel gameSettings;
+	private JPanel startPanel;
+	private JPanel titlePanel;
 	private HexBoard activeGameBoard;
 	private GameRunner runner;
 	private JButton startButton = new JButton("Start Game");
+	private ImageIcon gif = new ImageIcon("lib/GIVv.gif");
+	private JLabel gifLabel = new JLabel(gif);
+	private JLabel title = new JLabel("Hex Game", SwingConstants.CENTER);
+	private JLabel subtitle1 = new JLabel("David Jimenez, Dylan Williams, Jay Coughlon, Mason Richardson", SwingConstants.CENTER);
+	private JLabel subtitle2 = new JLabel("CSC445 Game Project, Spring 2023", SwingConstants.CENTER);
 	
 	public GUI() {
-		gameSettings = new JPanel();
+		gameSettings = new JPanel(new BorderLayout());
 		gameSettings.setPreferredSize(new Dimension(500, 500));
-		gameSettings.add(startButton);
+		startPanel = new JPanel(new FlowLayout());
+		titlePanel = new JPanel(new BorderLayout());
+		title.setFont(new Font("Courier New", Font.BOLD, 24));
+		subtitle1.setFont(new Font("Courier New", Font.BOLD, 12));
+		subtitle2.setFont(new Font("Courier New", Font.BOLD, 12));
+		startPanel.add(startButton);
+		
+		titlePanel.add(title, BorderLayout.NORTH);
+		titlePanel.add(subtitle1, BorderLayout.CENTER);
+		titlePanel.add(subtitle2, BorderLayout.SOUTH);
+		
+		gameSettings.add(titlePanel, BorderLayout.NORTH);
+		gameSettings.add(gifLabel, BorderLayout.CENTER);
+		gameSettings.add(startPanel, BorderLayout.SOUTH);
 		
 		startButton.addActionListener(this);	
 		
